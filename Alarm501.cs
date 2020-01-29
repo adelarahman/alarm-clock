1be675ab3d6bd80cee05fc78d822e5e01a1dcdf1
adelarahman <46939002+adelarahman@users.noreply.github.com>
2020-01-29 08:08:34 -0600

    project start

---
+++ Alarm501.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdelaRahman.CIS501.AlarmClock
+{
+    public partial class Alarm501 : Form
+    {
+        public Alarm501()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            AddEditAlarm form = new AddEditAlarm();
+            form.Show();
+        }
+    }
+}
